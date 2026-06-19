eb8a56fa827812238c189e0de58dd42ef30e048a
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 02:44:54 +0600

    add: added sliding window

---
+++ 05_Sliding_Windows/01_find_anagrams.cs
@@ -0,0 +1,23 @@
+using Solve;
+
+namespace SlidingWindows;
+
+public class FindAnagramsProblem : IProblem
+{
+    public string Category => "Sliding Windows";
+    public string Name => "Find Anagrams";
+
+    public IList<int> FindAnagrams(string s, string p)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = FindAnagrams(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
