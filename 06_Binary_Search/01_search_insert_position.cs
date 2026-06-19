3b6940ebae7ff156d24861bb2d742fa3196371db
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 02:45:07 +0600

    add: added Binary search category

---
+++ 06_Binary_Search/01_search_insert_position.cs
@@ -0,0 +1,24 @@
+using Solve;
+
+namespace BinarySearch;
+
+public class SearchInsertProblem : IProblem
+{
+    public string Category => "Binary Search";
+    public string Name => "Search Insert Position";
+
+    public int SearchInsert(int[] nums, int target)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = SearchInsert(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
+
