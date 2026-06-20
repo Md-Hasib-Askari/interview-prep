8bad32d82706b75d8a15f97fe5ff6b9f32301aaa
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 12:33:44 +0600

    add: stacks

---
+++ 07_Stacks/01_valid_parentheses.cs
@@ -0,0 +1,24 @@
+using Solve;
+
+namespace Stacks;
+
+public class IsValidProblem : IProblem
+{
+    public string Category => "Stacks";
+    public string Name => "Valid Parentheses";
+
+    public bool IsValid(string s)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = IsValid(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
+
