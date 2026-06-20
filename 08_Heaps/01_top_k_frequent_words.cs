2da91fadddcb094f100bab23d59cec1e80e6707c
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 12:33:49 +0600

    add: heaps

---
+++ 08_Heaps/01_top_k_frequent_words.cs
@@ -0,0 +1,24 @@
+using Solve;
+
+namespace Heaps;
+
+public class TopKFrequentProblem : IProblem
+{
+    public string Category => "Heaps";
+    public string Name => "Top K Frequent Words";
+
+    public IList<string> TopKFrequent(string[] words, int k)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = TopKFrequent(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
+
