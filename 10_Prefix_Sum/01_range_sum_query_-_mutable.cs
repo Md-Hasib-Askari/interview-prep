528e9f12e5fe79dc82ede5b4305153a6a2940a26
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 12:34:14 +0600

    add: prefix sum

---
+++ 10_Prefix_Sum/01_range_sum_query_-_mutable.cs
@@ -0,0 +1,29 @@
+using Solve;
+
+namespace PrefixSum;
+
+public class NumArray
+{
+    public NumArray(int[] nums) { }
+
+    public void Update(int index, int val) { }
+
+    public int SumRange(int left, int right)
+    {
+        return 0;
+    }
+}
+
+public class SumRangeProblem : IProblem
+{
+    public string Category => "Prefix Sum";
+    public string Name => "Range Sum Query - Mutable";
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = SumRange(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
