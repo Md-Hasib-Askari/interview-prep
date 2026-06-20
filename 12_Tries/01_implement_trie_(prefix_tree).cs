9cea7b1062ca6a109c896fb7b70c312ebb8a395a
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 12:34:27 +0600

    add: tries

---
+++ 12_Tries/01_implement_trie_(prefix_tree).cs
@@ -0,0 +1,34 @@
+using Solve;
+
+namespace Trees;
+
+public class Trie
+{
+    public Trie() { }
+
+    public void Insert(string word) { }
+
+    public bool Search(string word)
+    {
+        return false;
+    }
+
+    public bool StartsWith(string prefix)
+    {
+        return false;
+    }
+}
+
+public class ImplementTrieProblem : IProblem
+{
+    public string Category => "Trees";
+    public string Name => "Implement Trie (Prefix Tree)";
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = sol(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
