e74f587d72b9939493f2a6aff0d98174a27abff6
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 12:40:43 +0600

    Add Bit Manipulation: Number of 1 Bits stub

---
+++ 18_Bit_Manipulation/01_number_of_1_bits.cs
@@ -0,0 +1,23 @@
+using Solve;
+
+namespace BitManipulation;
+
+public class HammingWeightProblem : IProblem
+{
+    public string Category => "Bit Manipulation";
+    public string Name => "Number of 1 Bits";
+
+    public int HammingWeight(int n)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = HammingWeight(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
