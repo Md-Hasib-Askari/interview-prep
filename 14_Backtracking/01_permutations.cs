626e42054f172679ca4721fcc983139392d9c47b
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 12:40:40 +0600

    Add Backtracking: Permutations stub

---
+++ 14_Backtracking/01_permutations.cs
@@ -0,0 +1,24 @@
+using Solve;
+
+namespace Backtracking;
+
+public class PermuteProblem : IProblem
+{
+    public string Category => "Backtracking";
+    public string Name => "Permutations";
+
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = Permute(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
+
