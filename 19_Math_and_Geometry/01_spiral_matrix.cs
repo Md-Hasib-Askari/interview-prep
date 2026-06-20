42bb6c4236888c6a4009c640872b6e9d21ccae22
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 12:40:44 +0600

    Add Math and Geometry: Spiral Matrix stub

---
+++ 19_Math_and_Geometry/01_spiral_matrix.cs
@@ -0,0 +1,24 @@
+using Solve;
+
+namespace MathandGeometry;
+
+public class SpiralOrderProblem : IProblem
+{
+    public string Category => "Math And Geometry";
+    public string Name => "Spiral Matrix";
+
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = SpiralOrder(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
+
