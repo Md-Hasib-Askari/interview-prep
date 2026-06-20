f42d8bddb5769efd9a59ad00875fd4bd0096026c
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 12:40:42 +0600

    Add Greedy Algorithms: Jump Game stub

---
+++ 16_Greedy/01_jump_game.cs
@@ -0,0 +1,23 @@
+using Solve;
+
+namespace GreedyAlgorithms;
+
+public class CanJumpProblem : IProblem
+{
+    public string Category => "Greedy Algorithms";
+    public string Name => "Jump Game";
+
+    public bool CanJump(int[] nums)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = CanJump(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
