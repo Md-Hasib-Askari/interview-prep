e0e6f30b21d5cece56bdaf80c7b6e1a4c060d0e5
MdHasibAskari <mdhsbaskari@gmail.com>
2026-06-20 12:33:56 +0600

    add: intervals

---
+++ 09_Intervals/01_merge_intervals.cs
@@ -0,0 +1,24 @@
+using Solve;
+
+namespace Intervals;
+
+public class MergeProblem : IProblem
+{
+    public string Category => "Intervals";
+    public string Name => "Merge Intervals";
+
+    public int[][] Merge(int[][] intervals)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public void Run()
+    {
+        // TODO: Set up test input and call the algorithm
+        // var result = Merge(/* args */);
+        // Console.WriteLine(result);
+        throw new NotImplementedException();
+    }
+}
+
